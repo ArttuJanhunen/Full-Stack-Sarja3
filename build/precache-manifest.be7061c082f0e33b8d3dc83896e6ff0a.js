dfa28b14569455604fd2e33e5f21d96782ac8007
Janhunen Arttu P <ajanhune@cubbli18-gold.local>
2019-02-11 16:27:13 +0200

    kaikki on taas kunnossa

---
+++ build/precache-manifest.be7061c082f0e33b8d3dc83896e6ff0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c3ead8686e563917370",
+    "url": "/static/js/main.2c3ead86.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "2c3ead8686e563917370",
+    "url": "/static/css/main.8227870e.chunk.css"
+  },
+  {
+    "revision": "dbd5ce9217ee7710dd4cba09a8412701",
+    "url": "/index.html"
+  }
+];
